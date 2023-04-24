8551bf478f6e5ae56d86222a2d181d213dbc0022
Anton Veshkin <antonveshkin@gmail.com>
2023-04-24 22:39:16 +0300

    Add functions rate for Student and Reviewer. Task 6.2

--- main.py
+++ main.py
@@ -6,13 +6,32 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+    def rate_lect(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
+    
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,9 +40,3 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
