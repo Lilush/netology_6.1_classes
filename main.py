0a4a7d9658439c0f1fc7fa3af88e8bc3c538fc69
Anton Veshkin <antonveshkin@gmail.com>
2023-04-24 23:19:42 +0300

    Add descriptions for classes: Student, Lecturer, Reviewer by overriding methods __str__(). Task 6.2

--- main.py
+++ main.py
@@ -15,6 +15,21 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+        
+    def _average_rate(self):
+        all_grades = []
+        for grade in self.grades.values():
+            all_grades += grade
+        average_rate = sum(all_grades) / len(all_grades)
+        return round(average_rate, 1)
+    
+    def __str__(self) :
+        result = f'Имя: {self.name}\n'
+        result += f'Фамилия: {self.surname}\n'
+        result += f'Средняя оценка за домашнии задания: {self._average_rate()}\n'
+        result += f'Курсы в процессе обучения: {",".join(self.courses_in_progress)}\n'
+        result += f'Завершенные курсы: {",".join(self.finished_courses)}'
+        return result
 
 
 class Mentor:
@@ -29,9 +44,21 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def _average_rate(self):
+        all_grades = []
+        for grade in self.grades.values():
+            all_grades += grade
+        average_rate = sum(all_grades) / len(all_grades)
+        return round(average_rate, 1)
+    
+    def __str__(self) :
+        result = f'Имя: {self.name}\n'
+        result += f'Фамилия: {self.surname}\n'
+        result += f'Средняя оценка за лекции: {self._average_rate()}'
+        return result
+
 
 class Reviewer(Mentor):
-
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -40,3 +67,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+        
+    def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return result
+
+
+
